0651bf874ae6864087a43f1cd53e89808c13d44d
shaikhmuskan1024@gmail.com <shaikhmuskan1024@gmail.com>
2023-07-10 15:21:18 +0530

    third commit

--- components/Footer.js
+++ components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
                 <p className='text-2xl text-center font-bold text-slate-900 p-2'>Contact Me:</p>
                 <div className='flex gap-6 justify-center '>
                     {/* <a href='https://www.instagram.com/'><FiInstagram className='hover:-mt-1 hover:text-sky-800' /></a> */}
-                    <a href='https://github.com/muskan1024'><FiGithub className='hover:-mt-1 hover:text-white' /></a>
-                    <a href='https://www.linkedin.com/in/muskan-shaikh-057441255/'><FiLinkedin className='hover:-mt-1 hover:text-white' /></a>
-                    <a href='https://twitter.com/Muskan1024'><FiTwitter className='hover:-mt-1 hover:text-white' /></a>
-                    <a href='mailto:shaikhmuskan1024@gmail.com'><FiMail className='hover:-mt-1 hover:text-white' /></a>
+                    <a href='https://github.com/muskan1024'><FiGithub className='hover:-mt-2 hover:text-white' /></a>
+                    <a href='https://www.linkedin.com/in/muskan-shaikh-057441255/'><FiLinkedin className='hover:-mt-2 hover:text-white' /></a>
+                    <a href='https://twitter.com/Muskan1024'><FiTwitter className='hover:-mt-2 hover:text-white' /></a>
+                    <a href='mailto:shaikhmuskan1024@gmail.com'><FiMail className='hover:-mt-2 hover:text-white' /></a>
                 </div>
             </div>
         </div>
--- components/NavBar.js
+++ components/NavBar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='relative w-full h-fit bg-white shadow-2xl  opacity-90 z-40'>
+    <div className='relative w-full h-fit bg-white shadow-2xl opacity-90 z-40'>
         <div className='w-full h-full text-center p-2 flex justify-between  px-auto'>
-            <img src='/logo1.png' alt='logo' className='relative h-12 pl-5'/>
+            <a href='/'><img src='/logo1.png' alt='logo' className='relative h-12 pl-5'/></a>
             <div className=' grid gap-4 grid-cols-2 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 text-center '>
-            <a href='/'><h1 className='text-3xl hover:bg-gray-100 hover:rounded-3xl hover:py-1'>Home</h1></a>
-            <a href='allBlogs'><h1 className='text-3xl hover:bg-gray-100 hover:rounded-3xl hover:py-1'>Blogs</h1></a>
-            <a href='allCategories'><h1 className='text-3xl hover:bg-gray-100 hover:rounded-3xl hover:py-1 '>Categories</h1></a>
+            <a href='/'><h1 className='text-3xl hover:bg-slate-200 hover:rounded-3xl '>Home</h1></a>
+            <a href='allBlogs'><h1 className='text-3xl hover:bg-slate-200 hover:rounded-3xl '>Blogs</h1></a>
+            <a href='allCategories'><h1 className='text-3xl hover:bg-slate-200 hover:rounded-3xl  '>Categories</h1></a>
             <a href='admin'><button className='text-white font-bold text-xl rounded p-1 px-4 hover:bg-sky-700 hover:rounded-3xl hover:py-1 bg-sky-500'>For Admin</button></a>
             </div>
         </div>
